10195a90ddc021b8226ca58dd25b7a316f994983
robyng442 <robjgraham@gmail.com>
2018-11-05 16:41:40 +0000

    Redone version with person class

--- src/familyTree/Family.java
+++ src/familyTree/Family.java
@@ -1,8 +1,103 @@
 package familyTree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Family {
+    ArrayList<Person> fam = new ArrayList<>();
 
+    private Person getPerson(String name) {
+        for (Person person : this.fam) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public boolean male(String name) {
+        if (getPerson(name) == null) {
+            Person newPerson = new Person();
+            newPerson.setName(name);
+            newPerson.setGender("m");
+            fam.add(newPerson);
+            return true;
+        }
+        else {
+            Person person = getPerson(name);
+            if (person.gender.isEmpty()) {
+                person.setGender("m");
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public boolean isMale(String name) {
+        Person person = getPerson(name);
+        if (person.getGender().equals("m")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean female(String name) {
+        if (getPerson(name) == null) {
+            Person newPerson = new Person();
+            newPerson.setName(name);
+            newPerson.setGender("f");
+            fam.add(newPerson);
+            return true;
+        }
+        else {
+            Person person = getPerson(name);
+            if (person.gender.isEmpty()) {
+                person.setGender("f");
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    public boolean isFemale(String name) {
+        Person person = getPerson(name);
+        if (person.getGender() == "f") {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setParentOf(String childName, String parentName) {
+        Person parent = getPerson(parentName);
+        Person child = getPerson(childName);
+
+        if (parent == null) {
+            parent = new Person();
+            parent.setName(parentName);
+            this.fam.add(parent);
+        }
+        if (child == null) {
+            child = new Person();
+            child.setName(childName);
+            this.fam.add(child);
+        }
+
+        child.setParent(parentName);
+        parent.addChild(childName);
+        return true;
+
+    }
+    public String getParentsOf(String name) {
+        Person person = getPerson(name);
+        return Arrays.toString(person.getParents());
+    }
+
+    public String getChildrenOf(String name) {
+        Person person = getPerson(name);
+        String[] children = person.getChildren().toArray(new String[person.getChildren().size()]);
+        return Arrays.toString(children);
+    }
 
 }
 
--- src/familyTree/Person.java
+++ src/familyTree/Person.java
@@ -1,6 +1,48 @@
 package familyTree;
 
+import java.util.ArrayList;
 
 public class Person {
+    String name = "";
+    String gender = "";
+    String[] parents = new String[2];
 
+    ArrayList<String> children = new ArrayList<>();
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    public void setParent(String parentName) {
+        if (this.parents[0]== null) {
+            this.parents[0] = parentName;
+        }
+        else if (this.parents[1]==null) {
+            this.parents[1] = parentName;
+        }
+        else {
+
+        }
+    }
+
+    public void addChild(String child) {
+        this.children.add(child);
+    }
+
+    public void setGender(String newGender) {
+        this.gender = newGender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+    public String[] getParents() {
+        return this.parents;
+    }
+    public ArrayList<String> getChildren() {
+        return this.children;
+    }
 }
